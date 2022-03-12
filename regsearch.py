ae527ffbaced9035c4fe3f0ee3d39437cf3056c1
Evan Chu <49127693+readchu@users.noreply.github.com>
2022-03-12 13:21:00 -0500

    Add files via upload

---
+++ regsearch.py
@@ -0,0 +1,25 @@
+"""Prints lines and filenames of all files in provided directory that match
+the provided regular expression.
+
+Functions:
+    user_regex
+    user_directory
+    regex_searcher
+"""
+
+import re
+from pathlib import Path
+
+"""TODO: Function (or line?) for asking input and creating associated regular expression
+"""
+
+"""TODO: Function (or line?) for asking input and associated directory/WindowsPath
+"""
+
+"""TODO: Function for, with provided WindowsPath and regular expression,
+returns dictionary with keys being the filenames of all files that matched
+and values being lists of all lines that were in said files with matching expression
+with line number... Match #X, Line Y: Line text
+"""
+
+if __name__ = "__main__":
