ec2cf1b0d6075c90f45b9728d86ee5b75ad03c2f
Evan Chu <49127693+readchu@users.noreply.github.com>
2022-03-13 10:38:48 -0400

    update

--- regsearch.py
+++ regsearch.py
@@ -2,20 +2,31 @@
 the provided regular expression.
 
 Functions:
-    user_regex
     user_directory
     regex_searcher
 """
 
 import re
+import PyInputPlus as pyip
 from pathlib import Path
 
-"""TODO: Function (or line?) for asking input and creating associated regular expression
-"""
+def folder_exists(folder_name: str) -> Optional[Path]:
+"""Search all user directories for a folder and asks user to pick if multiple exist."""
+    home_directory = Path.home()
+    folder_holder = list(home_directory.rglob(folder_name))
+    folder = None
+    if len(folder_holder) == 1:
+        folder = folder_holder[0]
+    else:
+        for i, WindowsPath in enumerate(folder_holder):
+            folder_holder[i] = str(WindowsPath)
+        #pyip,inputMenu only accepts strings and returns strings
+        print("You have multiple folders with that name. Choose one of them. Tip: Use the numbers.\n")
+        folder_as_str = pyip.inputMenu(folder_holder, numbered=True)
+        folder = Path(folder_as_str)
+    return folder
 
-"""TODO: Function (or line?) for asking input and associated directory/WindowsPath
-"""
-
+def regex_searcher():
 """TODO: Function for, with provided WindowsPath and regular expression,
 returns dictionary with keys being the filenames of all files that matched
 and values being lists of all lines that were in said files with matching expression
